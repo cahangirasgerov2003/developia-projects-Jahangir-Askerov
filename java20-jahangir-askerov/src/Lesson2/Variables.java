f38264da92d768b2ea537e19444323b58677be48
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-08 03:19:52 +0400

    A class called Variables will be added

---
+++ java20-jahangir-askerov/src/Lesson2/Variables.java
@@ -0,0 +1,25 @@
+package Lesson2;
+
+public class Variables {
+    public static void main(String[] args){
+        int age = 7;
+        age = 14;
+        byte b = 7;
+        short s = 7;
+        long l = 7L;
+        double d = 7.9;
+        float f = 7.9F;
+        char c = 'A';
+        boolean hasHome = true;
+
+        System.out.println("age : " + age);
+        System.out.println("b : " + b);
+        System.out.println("s : " + s);
+        System.out.println("l : " + l);
+        System.out.println("d : " + d);
+        System.out.println("f : " + f);
+        System.out.println("c : " + c);
+        System.out.println("hasHome : " + hasHome);
+
+    }
+}
