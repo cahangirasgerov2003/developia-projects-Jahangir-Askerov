1320a8e0b074a2e08163d55495b8d0a606a4a90b
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-09 20:58:50 +0400

    Dərs 3 Operatorlara aid tapşırıq

---
+++ java20-jahangir-askerov/src/Lesson3/OperatorsTask.java
@@ -0,0 +1,12 @@
+package Lesson3;
+
+public class OperatorsTask {
+    public static void main(String[] args) {
+        int age = 20;
+        if(age > 15){
+            System.out.println("Boyukdur");
+        }else{
+            System.out.println("Kicikdir");
+        }
+    }
+}
