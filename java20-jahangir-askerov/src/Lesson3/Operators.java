670907cc08adbefef5cc0f90c23d903725da4338
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-11 22:59:36 +0400

    Lesson 3.  The task solution for operators will be changed

---
+++ java20-jahangir-askerov/src/Lesson3/Operators.java
@@ -0,0 +1,13 @@
+package Lesson3;
+
+public class Operators {
+    public static void main(String[] args) {
+        int age = 20;
+        int value = 15;
+        if(age > value){
+            System.out.println(age + "is taller than" + value);
+        }else{
+            System.out.println(age + "is smaller than" + value);
+        }
+    }
+}
