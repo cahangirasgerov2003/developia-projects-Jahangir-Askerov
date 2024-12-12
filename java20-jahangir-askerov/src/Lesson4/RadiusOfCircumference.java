ce87c1f3bbe0385ff928099f7cb24b60cc33a994
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-12 19:22:43 +0400

    Dərs 4. Çevrənin radiusunun hesablanması

---
+++ java20-jahangir-askerov/src/Lesson4/RadiusOfCircumference.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+public class RadiusOfCircumference {
+    public static void main(String[] args) {
+        double radius = 12.4;
+        double pi = 3.14;
+
+        double RadiusOfCircumference = 2 * pi * radius;
+
+        System.out.println("The Radius of circumference = " + RadiusOfCircumference);
+    }
+}
