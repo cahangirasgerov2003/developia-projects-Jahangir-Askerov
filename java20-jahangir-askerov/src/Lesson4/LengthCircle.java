80f9deca9d26bf6c8a3849a7227e96d36efa0382
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-12 19:27:49 +0400

    Dərs 4. Adla bağlı problem həll ediləcək

---
+++ java20-jahangir-askerov/src/Lesson4/LengthCircle.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+public class LengthCircle {
+    public static void main(String[] args) {
+        double radius = 12.4;
+        double pi = 3.14;
+
+        double lengthCircle = 2 * pi * radius;
+
+        System.out.println("The length of the circle = " + lengthCircle);
+    }
+}
