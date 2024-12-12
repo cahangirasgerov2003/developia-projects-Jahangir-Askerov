8fe2d9a6021b8b7521b36a32199ec4ae4d2e8c4d
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-12 19:18:47 +0400

    Lesson 3. Scanner class example

---
+++ java20-jahangir-askerov/src/Lesson3/PrimitiveTypeConversionRepeat.java
@@ -0,0 +1,12 @@
+package Lesson3;
+
+import java.util.Scanner;
+
+public class PrimitiveTypeConversionRepeat {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name ?");
+        String name = sc.nextLine();
+        System.out.println("Your name is " + name);
+    }
+}
