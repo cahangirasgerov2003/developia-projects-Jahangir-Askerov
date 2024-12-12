232449be5dbf040aaca44ad950fa653fe167451b
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-12 19:34:14 +0400

    Dərs 4. Ədədin cüt və ya tək olması

--- java20-jahangir-askerov/src/Lesson4/EvenOrOdd.java
+++ java20-jahangir-askerov/src/Lesson4/EvenOrOdd.java
@@ -1,5 +1,12 @@
 package Lesson4;
 
 public class EvenOrOdd {
-
+    public static void main(String[] args) {
+        int variable = 12;
+        if(variable % 2 == 0){
+            System.out.println("Bu eded cutdur");
+        }else{
+            System.out.println("Bu eded tekdir");
+        }
+    }
 }
