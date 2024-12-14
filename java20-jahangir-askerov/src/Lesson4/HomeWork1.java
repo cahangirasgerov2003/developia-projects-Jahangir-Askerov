96e2f440a1f35b5f41205bc971f225f6bf4abcbf
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-14 04:55:11 +0400

    Lesson 4. Homeworks solution codes will be added

---
+++ java20-jahangir-askerov/src/Lesson4/HomeWork1.java
@@ -0,0 +1,11 @@
+package Lesson4;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int a = 15;
+        do {
+            System.out.println(a);
+            a++;
+        }while (a<=40);
+    }
+}
---
+++ java20-jahangir-askerov/src/Lesson4/HomeWork2.java
@@ -0,0 +1,13 @@
+package Lesson4;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        int a = 20;
+        while (a <= 50){
+            if(a % 2 != 0){
+                System.out.println(a);
+            }
+            a++;
+        }
+    }
+}
