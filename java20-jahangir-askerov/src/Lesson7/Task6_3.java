b7b3eff5a3fe31344fbca80890894018c38c495d
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-23 20:58:24 +0400

    Lesson 7. Task 6_3 solution 2

--- java20-jahangir-askerov/src/Lesson7/Task6_3.java
+++ java20-jahangir-askerov/src/Lesson7/Task6_3.java
@@ -3,7 +3,13 @@ package Lesson7;
 public class Task6_3 {
     public static void main(String[] args) {
         int variable = 3;
+        int result = 1;
 
-        System.out.println(Math.pow(variable, 3));
+        for(int i=0; i<3; i++){
+            result = result * variable;
+        }
+
+        System.out.println(result);
+
     }
 }
