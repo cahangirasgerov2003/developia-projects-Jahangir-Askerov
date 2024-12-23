6e4d8e5f2d6f2423854657cb96aa21dd1f6c2d1c
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-23 20:56:33 +0400

    Lesson 7. Classes and objects task 6_3

---
+++ java20-jahangir-askerov/src/Lesson6/Task6.java
@@ -0,0 +1,7 @@
+package Lesson6;
+
+public class Task6 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java20-jahangir-askerov/src/Lesson7/Task6_3.java
@@ -0,0 +1,9 @@
+package Lesson7;
+
+public class Task6_3 {
+    public static void main(String[] args) {
+        int variable = 3;
+
+        System.out.println(Math.pow(variable, 3));
+    }
+}
