8146333450ae1db595f8a487043917cea886f9c0
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2024-12-30 13:12:09 +0400

    Dərs 8. forEach ilə Arrayin cəmlənməsi

--- java20-jahangir-askerov/src/Lesson8/ArrayDemo.java
+++ java20-jahangir-askerov/src/Lesson8/ArrayDemo.java
@@ -4,9 +4,12 @@ public class ArrayDemo {
     public static void main(String[] args) {
        int[] array = {12, 34, 1, 124};
        int sum = 0;
-       for (int i = 0; i< array.length; i++){
+       /* for (int i = 0; i< array.length; i++){
            sum+=array[i];
-       }
+       } */
+        for(int i : array){
+            sum += i;
+        }
        System.out.println("Sum = "+ sum);
     }
 }
