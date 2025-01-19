b222ac7d6159fa175da08904ed35f7cd800ef212
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2025-01-19 17:16:46 +0400

    Lesson 13. Homework ( Spring )

--- spring/spring-java20-jahangir-askerov/src/main/java/az/developia/spring_java20_jahangir_askerov/SpringJava20JahangirAskerovApplication.java
+++ spring/spring-java20-jahangir-askerov/src/main/java/az/developia/spring_java20_jahangir_askerov/SpringJava20JahangirAskerovApplication.java
@@ -2,12 +2,30 @@ package az.developia.spring_java20_jahangir_askerov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@RestController
 public class SpringJava20JahangirAskerovApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJava20JahangirAskerovApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJava20JahangirAskerovApplication.class, args);
+    }
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello world !";
+    }
+
+    @GetMapping(path = "/person/create")
+    public String createPerson() {
+        Person person1 = new Person("Jahangir", "Askerov");
+        String createdPerson = person1.toString();
+        System.out.println(createdPerson);
+        return createdPerson;
+    }
 
 }
