b0ff5ea6e74b2486b649847e0752121cf32f02eb
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2025-01-19 16:26:50 +0400

    Lesson 13. Spring project will be crated

---
+++ spring/spring-java20-jahangir-askerov/src/main/java/az/developia/spring_java20_jahangir_askerov/SpringJava20JahangirAskerovApplication.java
@@ -0,0 +1,13 @@
+package az.developia.spring_java20_jahangir_askerov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJava20JahangirAskerovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJava20JahangirAskerovApplication.class, args);
+	}
+
+}
---
+++ spring/spring-java20-jahangir-askerov/src/test/java/az/developia/spring_java20_jahangir_askerov/SpringJava20JahangirAskerovApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.spring_java20_jahangir_askerov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJava20JahangirAskerovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
