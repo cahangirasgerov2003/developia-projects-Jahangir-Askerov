1dd1b628fcb35c12f53cab166cb8245a9767db92
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2025-01-27 17:18:06 +0400

    Lesson 15. Mini-google ( Spring )

--- spring/spring-java20-jahangir-askerov/src/main/java/az/developia/spring_java20_jahangir_askerov/SpringJava20JahangirAskerovApplication.java
+++ spring/spring-java20-jahangir-askerov/src/main/java/az/developia/spring_java20_jahangir_askerov/SpringJava20JahangirAskerovApplication.java
@@ -1,15 +1,46 @@
 package az.developia.spring_java20_jahangir_askerov;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringJava20JahangirAskerovApplication {
+	String[] users = { "Cahangir", "Kamran", "Cuma", "Ali", "Azad" };
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJava20JahangirAskerovApplication.class, args);
+	}
+
+	@GetMapping(path = "")
+	public String sayHello() {
+		return "Welcome to our site !";
+	}
+
+	@GetMapping(path = "person")
+	public Person getUserData() {
+		Person person1 = new Person("Cahangir", "Asgerov");
+		return person1;
+	}
+
+	@GetMapping(path = "search")
+	public List<String> getSearchedUsers(
+			@RequestParam(name = "q", required = false, defaultValue = "") String userName) {
+		List<String> searchedUsers = new ArrayList<String>();
+		for (String user : users) {
+			if (user.contains(userName)) {
+				searchedUsers.add(user);
+			}
+		}
+
+		return searchedUsers;
+
 	}
 
 }
