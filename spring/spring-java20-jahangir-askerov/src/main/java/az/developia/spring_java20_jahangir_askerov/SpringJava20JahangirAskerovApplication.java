26c5f883cd7be05dfddb033c5e8ae39bd52feff6
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2025-01-27 18:59:19 +0400

    Lesson 15. Yeni annotasiyalar ( Spring )

--- spring/spring-java20-jahangir-askerov/src/main/java/az/developia/spring_java20_jahangir_askerov/SpringJava20JahangirAskerovApplication.java
+++ spring/spring-java20-jahangir-askerov/src/main/java/az/developia/spring_java20_jahangir_askerov/SpringJava20JahangirAskerovApplication.java
@@ -6,11 +6,13 @@ import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("root-controller")
 public class SpringJava20JahangirAskerovApplication {
 	String[] users = { "Cahangir", "Kamran", "Cuma", "Ali", "Azad" };
 
