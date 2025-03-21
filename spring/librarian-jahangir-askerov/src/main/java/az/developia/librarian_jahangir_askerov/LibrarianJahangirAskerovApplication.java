17f3095afd1ad951853fa118f9d2e919d3222ac1
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2025-03-21 18:40:49 +0400

    Lesson 25. Initial commit

---
+++ spring/librarian-jahangir-askerov/src/main/java/az/developia/librarian_jahangir_askerov/LibrarianJahangirAskerovApplication.java
@@ -0,0 +1,13 @@
+package az.developia.librarian_jahangir_askerov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarianJahangirAskerovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarianJahangirAskerovApplication.class, args);
+	}
+
+}
---
+++ spring/librarian-jahangir-askerov/src/test/java/az/developia/librarian_jahangir_askerov/LibrarianJahangirAskerovApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.librarian_jahangir_askerov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarianJahangirAskerovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
