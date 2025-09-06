f3b7be006cbee0827a23ee37a669df3516513ebe
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2025-09-06 23:46:12 +0400

    Feat: add regular download API

--- spring/librarian-jahangir-askerov/src/main/java/az/developia/librarian_jahangir_askerov/controller/FileController.java
+++ spring/librarian-jahangir-askerov/src/main/java/az/developia/librarian_jahangir_askerov/controller/FileController.java
@@ -1,21 +1,44 @@
 package az.developia.librarian_jahangir_askerov.controller;
 
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import az.developia.librarian_jahangir_askerov.exception.MyException;
+import az.developia.librarian_jahangir_askerov.util.io.FileContentReader;
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping(path = "/files")
 public class FileController {
+	
+	@Autowired
+	private FileContentReader contentReader;
+	
+	private Path rootLoacation;
+	
+	@PostConstruct
+	public void init() {
+		rootLoacation = Paths.get("C:/files");
+	}
 
 //	File upload
 	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -36,6 +59,42 @@ public class FileController {
 		String filenameNewVersion = originalFilename.replace(filenameWithoutExtension, uuid);
 		
 		Files.copy(stream, Paths.get("C:/files", filenameNewVersion), StandardCopyOption.REPLACE_EXISTING);
+	}
+	
+	@GetMapping("/download/raw/{filename:.+}")
+	public ResponseEntity<Resource> regularDownload(@PathVariable String filename){
+		
+		Resource file = getResourceByFilename(filename);
+		
+		if(file == null) {
+			throw new MyException(contentReader.readFromFile("fileNotFound.txt"), null, "FileNotFoundException");
+		}
+		
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename = \"" + filename + "\"" ).body(file);
+	}
+ 
+	private Resource getResourceByFilename(String filename) {
+		
+		try {
+			Path fullPath = getFileFullPath(filename);
+			
+			Resource resource = new UrlResource(fullPath.toUri());
+				
+			if(resource.isReadable() || resource.exists()) {
+				return resource;
+			}
+			
+		} catch (MalformedURLException e) {
+			e.getStackTrace();
+		}
+		
+		
+		
+		return null;
+	}
+
+	private Path getFileFullPath(String filename) {
+		return rootLoacation.resolve(filename);
 	}
 	
 }
